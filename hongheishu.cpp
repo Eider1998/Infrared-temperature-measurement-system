d46881a3991d9054c212d31b3d0b4459e473e810
Eider1998 <39902594+Eider1998@users.noreply.github.com>
2020-05-01 20:08:57 +0800

    hongheishu.cpp
    This is jingzhao's hongheishu.cpp code.

---
+++ hongheishu.cpp
@@ -0,0 +1,46 @@
+/* 
+ * 对红黑树的节点(y)进行右旋转
+ *
+ * 右旋示意图(对节点y进行左旋)：
+ *            py                               py
+ *           /                                /
+ *          y                                x                  
+ *         /  \      --(右旋)-->            /  \                     #
+ *        x   ry                           lx   y  
+ *       / \                                   / \                   #
+ *      lx  rx                                rx  ry
+ * 
+ */
+template <class T>
+void RBTree<T>::rightRotate(RBTNode<T>* &root, RBTNode<T>* y)
+{
+    // 设置x是当前节点的左孩子。
+    RBTNode<T> *x = y->left;
+
+    // 将 “x的右孩子” 设为 “y的左孩子”；
+    // 如果"x的右孩子"不为空的话，将 “y” 设为 “x的右孩子的父亲”
+    y->left = x->right;
+    if (x->right != NULL)
+        x->right->parent = y;
+
+    // 将 “y的父亲” 设为 “x的父亲”
+    x->parent = y->parent;
+
+    if (y->parent == NULL) 
+    {
+        root = x;            // 如果 “y的父亲” 是空节点，则将x设为根节点
+    }
+    else
+    {
+        if (y == y->parent->right)
+            y->parent->right = x;    // 如果 y是它父节点的右孩子，则将x设为“y的父节点的右孩子”
+        else
+            y->parent->left = x;    // (y是它父节点的左孩子) 将x设为“x的父节点的左孩子”
+    }
+
+    // 将 “y” 设为 “x的右孩子”
+    x->right = y;
+
+    // 将 “y的父节点” 设为 “x”
+    y->parent = x;
+}
